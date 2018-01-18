2850dbcd03004c6957a3b5cb6fe645f663cb9569
Non <Natchanon_angkool@cmu.ac.th>
2018-01-18 20:48:09 +0700

    First upload!!

---
+++ app/src/main/java/com/example/non/iparking/ResultActivity.kt
@@ -0,0 +1,60 @@
+package com.example.non.iparking
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.toast
+
+class ResultActivity : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
+    var mAuthListener: FirebaseAuth.AuthStateListener? = null
+    private val TAG:String = "Result Activity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val user = mAuth!!.currentUser
+
+        result_emailData.text = user!!.email
+        result_uidData.text = user.uid
+
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val users = firebaseAuth.currentUser
+            if (users == null) {
+                startActivity(Intent(this@ResultActivity, LoginActivity::class.java))
+                finish()
+            }
+        }
+
+        result_signOutBtn.setOnClickListener {
+            mAuth!!.signOut()
+            toast("Signed out!")
+            Log.d(TAG, "Signed out!")
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth!!.addAuthStateListener { mAuthListener }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mAuthListener != null) { mAuth!!.removeAuthStateListener { mAuthListener } }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) { moveTaskToBack(true) }
+        return super.onKeyDown(keyCode, event)
+    }
+}
