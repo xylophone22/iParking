8649db185e35d8b8f691654ddce146e162e7657b
Non <Natchanon_angkool@cmu.ac.th>
2018-01-22 21:20:05 +0700

    no message

--- app/src/main/java/com/CAMT/NSC/iparking/ResultActivity.kt
+++ app/src/main/java/com/CAMT/NSC/iparking/ResultActivity.kt
@@ -5,15 +5,22 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_result.*
 import org.jetbrains.anko.toast
+import com.CAMT.NSC.iparking.barcode.BarcodeCaptureActivity
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.vision.barcode.Barcode
 
 class ResultActivity : AppCompatActivity() {
 
+    private lateinit var mResultTextView: TextView
+
     var mAuth: FirebaseAuth? = null
     var mAuthListener: FirebaseAuth.AuthStateListener? = null
-    private val TAG:String = "Result Activity"
+    private val TAG: String = "Result Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +48,13 @@ class ResultActivity : AppCompatActivity() {
             startActivity(Intent(this@ResultActivity, MainActivity::class.java))
             finish()
         }
+
+        mResultTextView = findViewById(R.id.txtResult)
+
+        findViewById<Button>(R.id.scanBtn).setOnClickListener {
+            val intent = Intent(applicationContext, BarcodeCaptureActivity::class.java)
+            startActivityForResult(intent, BARCODE_READER_REQUEST_CODE)
+        }
     }
 
     override fun onStart() {
@@ -50,11 +64,36 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        if (mAuthListener != null) { mAuth!!.removeAuthStateListener { mAuthListener } }
+        if (mAuthListener != null) {
+            mAuth!!.removeAuthStateListener { mAuthListener }
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK) { moveTaskToBack(true) }
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true)
+        }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
+                    val p = barcode.cornerPoints
+                    mResultTextView.text = barcode.displayValue
+                } else
+                    mResultTextView.setText(R.string.no_barcode_captured)
+            } else
+                Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                        CommonStatusCodes.getStatusCodeString(resultCode)))
+        } else
+            super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        private val LOG_TAG = ResultActivity::class.java.simpleName
+        private val BARCODE_READER_REQUEST_CODE = 1
     }
 }
