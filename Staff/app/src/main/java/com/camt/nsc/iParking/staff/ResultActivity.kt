1f0c461e8518c7d9ed9288b8b0ba5e5465d4c74f
Non <Natchanon_angkool@cmu.ac.th>
2018-01-28 17:16:21 +0700

    no message

--- Staff/app/src/main/java/com/camt/nsc/iParking/staff/ResultActivity.kt
+++ Staff/app/src/main/java/com/camt/nsc/iParking/staff/ResultActivity.kt
@@ -1,12 +1,79 @@
 package com.camt.nsc.iParking.staff
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+//-----------------------------------------------------//
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.toast
 
+//-----------------------------------------------------//
 class ResultActivity : AppCompatActivity() {
+
+    //Firebase references
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
+    //UI elements
+    private var tvFirstName: TextView? = null
+    private var tvLastName: TextView? = null
+    private var tvEmail: TextView? = null
+    private var tvUID: TextView? = null
+
+    private val TAG: String = "Result Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        initialise()
+
+        result_signOutBtn.setOnClickListener {
+            mAuth!!.signOut()
+            toast("Signed out!")
+            Log.d(TAG, "Signed out!")
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+            finish()
+        }
+
     }
+
+    private fun initialise() {
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        tvFirstName = findViewById<View>(R.id.tv_first_name) as TextView
+        tvLastName = findViewById<View>(R.id.tv_last_name) as TextView
+        tvEmail = findViewById<View>(R.id.result_emailData) as TextView
+        tvUID = findViewById<View>(R.id.result_uidData) as TextView
+
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        val mUser = mAuth!!.currentUser
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+
+        tvEmail!!.text = mUser.email
+        tvUID!!.text = mUser.uid
+
+        mUserReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                tvFirstName!!.text = snapshot.child("firstName").value as String
+                tvLastName!!.text = snapshot.child("lastName").value as String
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+
+    }
+
 }
