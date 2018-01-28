9511c734dbb2555a22dee76886fe73259b98fdd8
Non <Natchanon_angkool@cmu.ac.th>
2018-01-28 15:12:17 +0700

    no message

---
+++ Staff/app/src/main/java/com/camt/nsc/iParking/staff/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.camt.nsc.iParking.staff
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
